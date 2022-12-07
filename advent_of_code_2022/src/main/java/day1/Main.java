53a5842408dc96fd93da780cda8da7188703542d
Catalin Scarlat <catalinscarlat@hotmail.com>
2022-12-07 18:24:48 +0200

    Solving Day 1, Part 1 - most calories

--- advent_of_code_2022/src/main/java/day1/Main.java
+++ advent_of_code_2022/src/main/java/day1/Main.java
@@ -1,7 +1,39 @@
 package day1;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.Buffer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("inputs/day1.txt"));
+
+        int calories = 0;
+        int mostCalories = 0;
+        String eachLine;
+//        int topThreeSum;
+//        int firstPos;
+//        int secondPos;
+//        int thirdPos;
+
+        while ((eachLine = bufferedReader.readLine()) != null) {
+            String cal = eachLine.trim();
+            if (cal.isBlank()) {
+                if (mostCalories < calories) {
+                    mostCalories = calories;
+                }
+                calories = 0;
+                continue;
+            }
+            calories += Integer.parseInt(cal);
+        }
+        if (calories > mostCalories) {
+            System.out.println(calories);
+        } else {
+            System.out.println(mostCalories);
+        }
     }
 }
